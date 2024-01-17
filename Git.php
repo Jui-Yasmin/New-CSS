cd380b52c4dde406fe3d7211349873ff6cb736a4
Jui-Yasmin <jui.yasmin83@gmail.com>
2024-01-17 14:48:58 +0600

    CMD & Git-bash note file

---
+++ Git.php
@@ -0,0 +1,10 @@
+# mkdir  = to make folder
+# touch  = to make file
+# cd     = to change directory(akta folder ba file theka onno kono folder ba file jayer jonno)
+# cd ../ = cd ../ (You can use this tag to back up your old folders )
+# ls     = list(akta folder ba file ki ki jinis asa ta dekhar jonno)
+# start  = to open a file ( ex- start one.txt )
+# start. = start . folder name (to details a folder)
+# open   = to open a folder
+# git status = ki ki status obosta acha ta dekha 
+# 
